e310ad27e6ae16fe23f8e3e5bcc0ad8dd0d98234
Tomas Manguel <tmanguel@mobilecomputing.com.ar>
2022-03-23 20:06:14 -0300

    change network automatically

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,30 @@ function App() {
   }, [isAuthenticated, isWeb3Enabled]);
 
 
+  // asks the user for permission to change network to polygon mumbain testnet if other network is detected
+  useEffect(() => {
+    (async () => {
+      await window.ethereum.request({
+        id: 1,
+        jsonrpc: "2.0",
+        method: "wallet_addEthereumChain",
+        params: [
+          {
+            chainId: "0x13881",
+            rpcUrls: ["https://rpc-mumbai.maticvigil.com"],
+            chainName: "Polygon Testnet Mumbai",
+            nativeCurrency: {
+              name: "tMATIC",
+              symbol: "tMATIC", // 2-6 characters long
+              decimals: 18,
+            },
+            blockExplorerUrls: ["https://mumbai.polygonscan.com/"],
+          },
+        ],
+      });
+    })();
+  }, []);
+
   function TopBar() {
     return (
       <div>
